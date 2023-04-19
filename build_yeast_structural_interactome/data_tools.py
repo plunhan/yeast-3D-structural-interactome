e1fa44521d5a373d5a4de3d2f721897a11589c4e
Yilun Han <38421175+plunhan@users.noreply.github.com>
2023-04-19 00:19:12 -0400

    Modified data_tools.py

--- build_yeast_structural_interactome/data_tools.py
+++ build_yeast_structural_interactome/data_tools.py
@@ -2,14 +2,11 @@ import pandas as pd
 import numpy as np
 import pickle
 
-def process_BIOGRID_protein_interactome(InPath, OutPath, species='cerevisiae'): 
+def process_BIOGRID_protein_interactome(InPath, OutPath): 
+    # This function retrieves all PPIs within Saccharomyces cerevisiae S288C, and all other PPIs are discarded. 
     BIOGRID = pd.read_table(InPath)
-    if species == 'cerevisiae': 
-        species = 'Saccharomyces cerevisiae (S288c)'
-        organismID = 559292
-    elif species == 'pombe': 
-        species = 'Schizosaccharomyces pombe (972h)'
-        organismID = 284812
+    species = 'Saccharomyces cerevisiae (S288c)'
+    organismID = 559292
     BIOGRID = BIOGRID[BIOGRID['Experimental System Type'] == 'physical']
     BIOGRID = BIOGRID[(BIOGRID['Organism ID Interactor A'] == organismID) & (BIOGRID['Organism ID Interactor B'] == organismID)]
     BIOGRID = BIOGRID[(BIOGRID['SWISS-PROT Accessions Interactor A'] != '-') & (BIOGRID['SWISS-PROT Accessions Interactor B'] != '-')]
@@ -20,6 +17,9 @@ def process_BIOGRID_protein_interactome(InPath, OutPath, species='cerevisiae'):
     BIOGRID.to_csv(OutPath, sep='\t', index=False)
 
 def process_SGA_reliable(InPath, OutPath):
+    # This function retrieves high-quality (p-value <= 0.05) genetic interactions. 
+    # If a pair of genes has more than one high-quality genetic interactions, the genetic interactio score with the smallest p-value is retained. 
+    # All other entries are discarded. 
     entryDict = {} # the dict is used to compare different entries for a specific gene pair
     with open(InPath, 'r') as f:
         f.readline()
@@ -38,7 +38,8 @@ def process_SGA_reliable(InPath, OutPath):
     entryDf = pd.DataFrame().from_dict(entryDict, orient='index', columns=['query', 'array', 'GI', 'pvalue']).reset_index()
     entryDf.to_csv(OutPath, sep='\t')
 
-def merge_SGA(InPathList, OutPath):
+def merge_SGA(InPathList, OutPath): 
+    # This function merges multiple genetic interaction dataframes into one. 
     DataFrameList = []
     for i in range(len(InPathList)):
         df = pd.read_table(InPathList[i])
@@ -49,6 +50,8 @@ def merge_SGA(InPathList, OutPath):
     output.to_csv(OutPath, sep='\t', index=False)
 
 def generate_GI_matrix(InPath, OutPath): 
+    # This function builds a symmetric genetic interaction matrix of which each row and column is a genetic interaction profile. 
+    # Only high-quality (p-value <= 0.05) genetic interactions will be retained. 
     GI = pd.read_table(InPath)
     geneList = list(set(GI['query']).union(set(GI['array'])))
     geneList.sort()
@@ -60,6 +63,38 @@ def generate_GI_matrix(InPath, OutPath):
         GI_matrix.loc[row['array'], row['query']] = GI_score
         if index % 10 == 0: 
             print('{} out of {} queries have been processed. '.format(index, len(GI)))
+    GI_matrix.to_csv(OutPath, sep='\t')
+
+def generate_GI_full_matrix(InPathList, OutPath): 
+    # This function builds a symmetric genetic interaction matrix of which each row and column is a genetic interaction profile. 
+    # All genetic interactions will be retained without quality control. 
+    entryDict = {}
+    for InPath in InPathList: 
+        with open(InPath, 'r') as f: 
+            flag = 0
+            for line in f.readlines():
+                if flag == 0: 
+                    flag = 1
+                    continue
+                line = line.strip().split('\t')
+                query, array, GI, pvalue = line[0].split('_')[0], line[2].split('_')[0], line[5], line[6]
+                query, array = min(query, array), max(query, array)
+                index = '_'.join([query, array])
+                if (index in entryDict.keys()) and (float(pvalue) > entryDict[index][3]):
+                    continue
+                entryDict[index] = [query, array, float(GI), float(pvalue)]
+    GI = pd.DataFrame().from_dict(entryDict, orient='index', columns=['query', 'array', 'GI', 'pvalue']).reset_index()
+    geneList = list(set(GI['query']).union(set(GI['array'])))
+    geneList.sort()
+    zero_data = np.zeros(shape=(len(geneList), len(geneList)))
+    GI_matrix = pd.DataFrame(zero_data, columns=geneList, index=geneList)
+    GI_matrix[:] = np.nan
+    count = 0
+    for index, row in GI.iterrows(): 
+        GI_matrix.loc[row['query'], row['array']] = row['GI']
+        GI_matrix.loc[row['array'], row['query']] = row['GI']
+        count = count + 1
+        print('\r{:.2f}%% genetic interaction scores have been processed. '.format(count * 100 / len(GI)), end='')
     GI_matrix.to_csv(OutPath, sep='\t')
 
 def generate_essential_gene_list(OutPath): 
@@ -93,34 +128,3 @@ def generate_essential_gene_list(OutPath):
     for row in query.rows():
         essentialGeneList.append(row["genes.secondaryIdentifier"])
     pickle.dump(essentialGeneList, open(OutPath, 'wb'))
-
-def generate_GI_full_matrix(InPathList, OutPath): 
-    # create an empty matrix to record genetic interaction scores
-    entryDict = {}
-    for InPath in InPathList: 
-        with open(InPath, 'r') as f: 
-            flag = 0
-            for line in f.readlines():
-                if flag == 0: 
-                    flag = 1
-                    continue
-                line = line.strip().split('\t')
-                query, array, GI, pvalue = line[0].split('_')[0], line[2].split('_')[0], line[5], line[6]
-                query, array = min(query, array), max(query, array)
-                index = '_'.join([query, array])
-                if (index in entryDict.keys()) and (float(pvalue) > entryDict[index][3]):
-                    continue
-                entryDict[index] = [query, array, float(GI), float(pvalue)]
-    GI = pd.DataFrame().from_dict(entryDict, orient='index', columns=['query', 'array', 'GI', 'pvalue']).reset_index()
-    geneList = list(set(GI['query']).union(set(GI['array'])))
-    geneList.sort()
-    zero_data = np.zeros(shape=(len(geneList), len(geneList)))
-    GI_matrix = pd.DataFrame(zero_data, columns=geneList, index=geneList)
-    GI_matrix[:] = np.nan
-    count = 0
-    for index, row in GI.iterrows(): 
-        GI_matrix.loc[row['query'], row['array']] = row['GI']
-        GI_matrix.loc[row['array'], row['query']] = row['GI']
-        count = count + 1
-        print('\r{:.2f}%% genetic interaction scores have been processed. '.format(count * 100 / len(GI)), end='')
-    GI_matrix.to_csv(OutPath, sep='\t')
